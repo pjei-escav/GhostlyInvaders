6965706cd245990aba59303a8d80b9a3632c0ca4
Pablo Cartamil <450423dgsfgds@users.noreply.github.com>
2019-01-11 17:33:31 +0100

    Initial commit

---
+++ Assets/Scripts/MovimientoPacMan.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPacMan : MonoBehaviour
+{
+
+    Vector2 PosicionActual;
+    Vector2 SegundoVector;
+    Rigidbody2D rb;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SegundoVector = Vector2(0, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MueveDerecha()
+    {
+        rb.GetVector
+    }
+
+
+}
