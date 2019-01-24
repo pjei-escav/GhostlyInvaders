fe5d6a4ac38fa34fabfa711da99c8ac75481b314
Pablo Cartamil <450423dgsfgds@users.noreply.github.com>
2019-01-24 18:12:11 +0100

    Para migue

---
+++ Assets/Scripts/MovimientoFantasmas.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoFantasmas : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    Vector2 PosicionActual;
+    Vector2 PosicionFinal;
+
+    Vector2 VectorDerecha;
+    Vector2 VectorIzquierda;
+    Vector2 VectorAbajo;
+
+    bool moviendoDerecha = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        VectorDerecha = new Vector2(0.1f, 0f);
+        VectorIzquierda = -VectorDerecha;
+        VectorAbajo = new Vector2(1f, 0f);
+
+        MovimientoIzquierda();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PosicionActual = rb.position;
+
+        if (moviendoDerecha == true)
+        {
+            MovimientoDerecha();
+        }
+        else
+        {
+            MovimientoIzquierda();
+        }
+    }
+
+
+    void MovimientoIzquierda()
+    {
+        PosicionFinal = PosicionActual + VectorIzquierda;
+        rb.transform.position = PosicionFinal;
+    }
+
+    void MovimientoDerecha()
+    {
+        PosicionFinal = PosicionActual + VectorDerecha;
+        rb.transform.position = PosicionFinal;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("LimiteIzquierda"))
+        {
+            moviendoDerecha = true;
+            rb.transform.position = PosicionActual + VectorAbajo;
+        }
+        if (collision.gameObject.CompareTag("LimiteDerecha"))
+        {
+            moviendoDerecha = false;
+            rb.transform.position = PosicionActual + VectorAbajo;
+        }
+    }
+
+}
