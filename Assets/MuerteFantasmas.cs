93c6877211c4a5132214ee07cc6e62aede074402
Thesolidpakman <pablocartam@gmail.com>
2019-02-17 19:07:25 +0100

    Muerte de fantasmas

---
+++ Assets/MuerteFantasmas.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuerteFantasmas : MonoBehaviour
+{
+
+    Animator animacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animacion = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("DisparoPacMan"))
+        {
+            animacion.SetBool("muerto", true);
+            Invoke("Muerte", 1);
+        }
+    }
+
+
+    void Muerte()
+    {
+        Destroy(gameObject);
+    }
+
+}
--- Assets/Scripts/MovimientoDisparo.cs
+++ Assets/Scripts/MovimientoDisparo.cs
@@ -28,4 +28,17 @@ public class MovimientoDisparo : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fantasma"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+
+
 }
